849ee096e170db079c016e4e7fd614719a4f1b0e
Alwin Arrasyid <alwin.ridd@gmail.com>
2018-03-25 20:31:02 +0700

    Added few test case.

---
+++ test/decode.test.js
@@ -0,0 +1,63 @@
+const lpp = require(`../src`);
+
+test(`decode temperature data`, () => {
+  const decoded = lpp.decode(Buffer.from(`03670110`, `hex`));
+  const data = decoded[0];
+
+  expect(decoded.length).toEqual(1);
+  expect(data.data).toEqual(27.2);
+  expect(data.type).toEqual(`IPSO_TEMPERATURE_SENSOR`);
+  expect(data.size).toEqual(2);
+  expect(data.channel).toEqual(3);
+});
+
+test(`decode multiple temperature data`, () => {
+  const decoded = lpp.decode(Buffer.from(`03670110056700FF`, `hex`));
+  let data = decoded[0];
+
+  expect(decoded.length).toEqual(2);
+  expect(data.data).toEqual(27.2);
+  expect(data.type).toEqual(`IPSO_TEMPERATURE_SENSOR`);
+  expect(data.size).toEqual(2);
+  expect(data.channel).toEqual(3);
+
+  data = decoded[1];
+  expect(data.data).toEqual(25.5);
+  expect(data.type).toEqual(`IPSO_TEMPERATURE_SENSOR`);
+  expect(data.size).toEqual(2);
+  expect(data.channel).toEqual(5);
+});
+
+test(`decode accelerometer data`, () => {
+  const decoded = lpp.decode(Buffer.from(`067104D2FB2E0000`, `hex`));
+  const data = decoded[0];
+
+  const expectedData = [
+    1.234,
+    -1.234,
+    0
+  ];
+
+  expect(decoded.length).toEqual(1);
+  expect(data.data).toEqual(expect.arrayContaining(expectedData));
+  expect(data.type).toEqual(`IPSO_ACCELEROMETER`);
+  expect(data.size).toEqual(6);
+  expect(data.channel).toEqual(6);
+});
+
+test(`decode GPS data`, () => {
+  const decoded = lpp.decode(Buffer.from(`018806765ff2960a0003e8`, `hex`));
+  const data = decoded[0];
+
+  const expectedData = [
+    42.3519,
+    -87.9094,
+    10,
+  ];
+
+  expect(decoded.length).toEqual(1);
+  expect(data.data).toEqual(expect.arrayContaining(expectedData));
+  expect(data.type).toEqual(`IPSO_GPS_LOCATION`);
+  expect(data.size).toEqual(9);
+  expect(data.channel).toEqual(1);
+});
