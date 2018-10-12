1da71d7c450b00c9b1f748804c08d81f77132ab4
Alwin Arrasyid <alwin.ridd@gmail.com>
2018-10-12 10:42:27 +0700

    new int16 type

--- src/index.js
+++ src/index.js
@@ -89,6 +89,9 @@ function decode(buffer) {
       case payload.TYPES.IPSO_GENERIC_UINT16:
         data = slice.readUInt16BE();
         break;
+      case payload.TYPES.IPSO_GENERIC_INT16:
+        data = slice.readInt16BE();
+        break;
     }
 
     cursor += length;
